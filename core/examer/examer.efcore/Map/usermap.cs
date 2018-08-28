7abf9cd2c8b9c3d7fce1193d832beac93a9407b0
川 青 <ncqingchuan@126.com>
2018-08-28 20:18:32 +0800

    create table

--- core/examer/examer.efcore/Map/usermap.cs
+++ core/examer/examer.efcore/Map/usermap.cs
@@ -4,14 +4,15 @@ using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using examer.entities;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
 namespace examer.efcore.Map
 {
     internal class usermap : IEntityTypeConfiguration<userentity>
     {
         public void Configure(EntityTypeBuilder<userentity> builder)
         {
-            builder.HasKey(t => t.key).HasName("user_id");
-            builder.Property(t => t.key).ValueGeneratedOnAdd();
+            builder.HasKey(t => t.key);
+            builder.Property(t => t.key).ValueGeneratedOnAdd().HasColumnName("user_id");
             builder.ToTable("user");
         }
     }
