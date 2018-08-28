6fa4a402cefab206cebdfa2d6848a3c6f8c8a01b
zhaoxi <zhaoxi@uoko.com>
2018-08-28 10:37:52 +0800

    新增java项目

---
+++ java/examer/src/main/java/com/danny/examer/ExamerApplication.java
@@ -0,0 +1,12 @@
+package com.danny.examer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamerApplication.class, args);
+    }
+}
